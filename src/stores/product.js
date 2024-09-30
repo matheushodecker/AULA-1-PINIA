4afe315e99aa8a61933856c42487c982e2dc228d
matheushodecker <matheus.hodecker.santos@gmail.com>
2024-09-30 16:23:04 -0300

    aaaa

--- src/stores/product.js
+++ src/stores/product.js
@@ -3,21 +3,21 @@ import { defineStore } from 'pinia'
 
 export const useProductStore = defineStore('product', () => {
   const products = ref([
-    {id: 1, name: 'Harry potter', price: 59.99, qty:100},
-    {id: 2, name: 'a', price: 19.99, qty:100},
-    {id: 3, name: 'o', price: 59.99, qty:100}
+    { id: 1, name: 'Harry potter', price: 59.99, qty: 100 },
+    { id: 2, name: 'a', price: 19.99, qty: 100 },
+    { id: 3, name: 'o', price: 59.99, qty: 100 }
   ])
 
-function getProductsById(id) {
-  for (let product of products.value){
-    if(product.id == id){
-      return product
+  function getProductById(id) {
+    for (let product of products.value) {
+      if (product.id == id) {
+        return product
+      }
+    }
+    return null
+
+    //return porducts.value.find((product) => product.id == id)
   }
-}
-return null
 
-//return porducts.value.find((product) => product.id == id)
-}
-
-  return { products, getProductsById }
+  return { products, getProductById }
 })
